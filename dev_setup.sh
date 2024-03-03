23eda851e10515adb81797691ec1235f0e1b8c7b
chLi <565353780@qq.com>
2024-03-03 19:37:14 +0800

    add setup bash files

---
+++ dev_setup.sh
@@ -0,0 +1,6 @@
+pip install cython imageio numpy matplotlib pandas pillow \
+	pyembree pytest pyyaml scikit-image scipy tensorboardx \
+	tqdm trimesh h5py plyfile
+
+# pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cu118
+pip install torch-scatter -f https://data.pyg.org/whl/torch-2.1.2+cu118.html
